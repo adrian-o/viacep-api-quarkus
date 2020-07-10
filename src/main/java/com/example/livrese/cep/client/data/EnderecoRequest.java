94d66c22737e8f9f793bb3a4642c6c8486f86e2a
Adriano Ohana <adriano.ohana@gmail.com>
2020-07-10 20:59:39 -0300

    Deixando a request sem não persistente no DB

--- src/main/java/com/example/livrese/cep/client/data/EnderecoRequest.java
+++ src/main/java/com/example/livrese/cep/client/data/EnderecoRequest.java
@@ -1,12 +1,6 @@
 package com.example.livrese.cep.client.data;
 
-import java.util.Date;
-
-import io.quarkus.mongodb.panache.MongoEntity;
-import io.quarkus.mongodb.panache.PanacheMongoEntity;
-
-@MongoEntity(collection="EnderecoRequest")
-public class EnderecoRequest extends PanacheMongoEntity {
+public class EnderecoRequest {
     public String nome;
     public String cep;
 }
